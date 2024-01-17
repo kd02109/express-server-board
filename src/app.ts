4cd18aae3111c5ccfbeb2790f3a20d6b55ce9250
JUNSEOK <kd02109@gmail.com>
2024-01-17 21:32:06 +0900

    feat: middleware 초기 설정
    - cookie-parser
    - express-session
    - boay-parser

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,38 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
 
 dotenv.config();
-
+const { NODE_ENV } = process.env;
 const app = express();
 
 app.set('port', process.env.PORT || 8080);
+
+app.use((req, res, next) => {
+  if (NODE_ENV === 'development') {
+    morgan('dev')(req, res, next);
+  } else {
+    morgan('combined')(req, res, next);
+  }
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: 'session-cookie',
+  }),
+);
 
 app.get('/', (req, res) => {
   return res.send('HELLO EXPRESS');
